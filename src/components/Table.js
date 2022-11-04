f946c0900345e7cbc89ad4c3e4daef751649bc7d
Beatriz <beatriz.mediatto2@gmail.com>
2022-11-04 13:50:03 -0300

    requisito 7 concluido

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((element, i) => (
+            <tr key={ i }>
+              <td>
+                {element.description}
+              </td>
+              <td>{element.tag}</td>
+              <td>{element.method}</td>
+              <td>{Number(element.value).toFixed(2)}</td>
+              <td>{element.exchangeRates[element.currency].name}</td>
+              <td>{Number(element.exchangeRates[element.currency].ask).toFixed(2)}</td>
+              <td>
+                {Number(element.value * element.exchangeRates[element.currency].ask)
+                  .toFixed(2)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {}.isRequired;
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
