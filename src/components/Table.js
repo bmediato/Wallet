88708a82a1e311a7db2acda436dc190a0b3dd9fd
Beatriz <beatriz.mediatto2@gmail.com>
2023-05-18 11:15:42 -0300

    style: tabela

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { deleteItem } from '../redux/actions';
+import './css/Table.css';
 
 class Table extends Component {
   btnDelete = (element) => {
@@ -17,59 +18,57 @@ class Table extends Component {
         <table className="table">
           <thead className="thead-container">
             <tr>
-              <th className="description">Descrição</th>
-              <th className="tag">Tag</th>
-              <th className="method">Método de pagamento</th>
-              <th className="value">Valor</th>
-              <th className="coin">Moeda</th>
-              <th className="cambio">Câmbio utilizado</th>
-              <th className="convertedValue">Valor convertido</th>
-              <th className="convertedCoin">Moeda de conversão</th>
-              <th className="edit-delete">Editar/Excluir</th>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
             </tr>
           </thead>
-          <tbody>
-            <div className="tbody-container">
-              {expenses.map((element) => (
-                <tr key={ element.id } className="tr">
-                  <td className="element-description">
-                    {element.description}
-                  </td>
-                  <td className="element-tag">{element.tag}</td>
-                  <td className="element-method">{element.method}</td>
-                  <td className="element-value">{Number(element.value).toFixed(2)}</td>
-                  <td className="element-name">
-                    {element.exchangeRates[element.currency].name}
-                  </td>
-                  <td className="element-coin">
-                    {Number(element.exchangeRates[element.currency].ask).toFixed(2)}
-                  </td>
-                  <td className="element-converted">
-                    {Number(element.value * element.exchangeRates[element.currency].ask)
-                      .toFixed(2)}
-                  </td>
-                  <td className="element-real">Real</td>
-                  <td className="buttons">
-                    <button
-                      data-testid="edit-btn"
-                      type="button"
-                      className="btn-edit"
-                    >
-                      Editar
-                      {' '}
-                    </button>
-                    <button
-                      data-testid="delete-btn"
-                      type="button"
-                      className="btn-delete"
-                      onClick={ () => this.btnDelete(element) }
-                    >
-                      Excluir
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </div>
+          <tbody className="tbody-container">
+            {expenses.map((element) => (
+              <tr key={ element.id } className="tr">
+                <td className="element-description">
+                  {element.description}
+                </td>
+                <td className="element-tag">{element.tag}</td>
+                <td className="element-method">{element.method}</td>
+                <td className="element-value">{Number(element.value).toFixed(2)}</td>
+                <td className="element-name">
+                  {element.exchangeRates[element.currency].name}
+                </td>
+                <td className="element-coin">
+                  {Number(element.exchangeRates[element.currency].ask).toFixed(2)}
+                </td>
+                <td className="element-converted">
+                  {Number(element.value * element.exchangeRates[element.currency].ask)
+                    .toFixed(2)}
+                </td>
+                <td className="element-real">Real</td>
+                <td className="buttons">
+                  <button
+                    data-testid="edit-btn"
+                    type="button"
+                    className="btn-edit"
+                  >
+                    Editar
+                    {' '}
+                  </button>
+                  <button
+                    data-testid="delete-btn"
+                    type="button"
+                    className="btn-delete"
+                    onClick={ () => this.btnDelete(element) }
+                  >
+                    Excluir
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
 
         </table>
